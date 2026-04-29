5c83912818d1dfa4ea0d4723b5bcca8299a50f92
Dzaki Althalsyah <dz4ic7@gmail.com>
2026-04-30 01:26:38 +0700

    fix: docker-compose, entrypoint.sh add:.gitattributes

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-echo "🚀 Menjalankan ETL Pipeline (Bronze → Silver → Gold)..."
+echo "Menjalankan ETL Pipeline (Bronze → Silver → Gold)..."
 python etl/run_pipeline.py
 
-echo "✅ ETL selesai. DuckDB siap digunakan Metabase."
+echo "ETL selesai. DuckDB siap digunakan Metabase."
